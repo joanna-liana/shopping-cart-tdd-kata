9e41645a720f6d54132f5e77867234fc2c4770dc
Joanna Liana <joanna_liana@outlook.com>
2023-09-17 13:33:30 +0200

    Refactor naming

--- tests/priceCalculator.test.ts
+++ tests/priceCalculator.test.ts
@@ -69,7 +69,7 @@ describe('Price calculator', () => {
       1.36
     ]
   ])('returns the price per unit', (item, expectedPrice) => {
-    const pricePerUnit = calculatePricePerUnitOf(item);
+    const pricePerUnit = calculateUnitPriceOf(item);
 
     expect(pricePerUnit).toEqual(expectedPrice);
   });
@@ -106,14 +106,14 @@ function roundUp(originalNumber: number) {
   return Math.ceil(originalNumber * 100) / 100;
 }
 
-function calculatePricePerUnitOf(item: Item) {
+function calculateUnitPriceOf(item: Item) {
   const pricePerUnit = (item.cost * item.revenue) + item.cost;
 
   return roundUp(pricePerUnit);
 }
 
 function calculateFinalPriceOf(item: Item) {
-  const unitPrice = calculatePricePerUnitOf(item);
+  const unitPrice = calculateUnitPriceOf(item);
   const taxValue = Number((unitPrice * item.tax).toPrecision(2));
   const finalPrice = unitPrice + taxValue;
 
