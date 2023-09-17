34200958e5cd33d948e3e8c0438cb832695abf0c
Joanna Liana <joanna_liana@outlook.com>
2023-09-17 13:13:48 +0200

    Add failing tests for final price

--- tests/priceCalculator.test.ts
+++ tests/priceCalculator.test.ts
@@ -1,43 +1,53 @@
 describe('Price calculator', () => {
+  const ICEBERG = {
+    name: 'Iceberg',
+    cost: 1.55,
+    revenue: 0.15
+  };
+
+  const TOMATO = {
+    name: 'Tomato',
+    cost: 0.52,
+    revenue: 0.15
+  };
+
+  const CHICKEN = {
+    name: 'Chicken',
+    cost: 1.34,
+    revenue: 0.12
+  };
+
+  const BREAD = {
+    name: 'Bread',
+    cost: 0.71,
+    revenue: 0.12
+  };
+
+  const CORN = {
+    name: 'Corn',
+    cost: 1.21,
+    revenue: 0.12
+  };
+
   it.each([
     [
-      {
-        name: 'Iceberg',
-        cost: 1.55,
-        revenue: 0.15
-      },
+      ICEBERG,
       1.79
     ],
     [
-      {
-        name: 'Tomato',
-        cost: 0.52,
-        revenue: 0.15
-      },
+      TOMATO,
       0.60
     ],
     [
-      {
-        name: 'Chicken',
-        cost: 1.34,
-        revenue: 0.12
-      },
+      CHICKEN,
       1.51
     ],
     [
-      {
-        name: 'Bread',
-        cost: 0.71,
-        revenue: 0.12
-      },
+      BREAD,
       0.80
     ],
     [
-      {
-        name: 'Corn',
-        cost: 1.21,
-        revenue: 0.12
-      },
+      CORN,
       1.36
     ]
   ])('returns the price per unit', (item, expectedPrice) => {
@@ -45,7 +55,36 @@ describe('Price calculator', () => {
 
     expect(pricePerUnit).toEqual(expectedPrice);
   });
+
+  it.each([
+    [
+      ICEBERG,
+      2.17
+    ],
+    [
+      TOMATO,
+      0.73
+    ],
+    [
+      CHICKEN,
+      1.83
+    ],
+    [
+      BREAD,
+      0.88
+    ],
+    [
+      CORN,
+      1.50
+    ]
+  ])('returns the final item  price', (item, expectedPrice) => {
+    const pricePerUnit = calculateFinalPriceOf(item);
+
+    expect(pricePerUnit).toEqual(expectedPrice);
+  });
 });
+
+
 
 type Item = {
   name: string;
